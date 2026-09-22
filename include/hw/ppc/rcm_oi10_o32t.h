d01b20eaa197c7e50df16b1aa7c4279e95e37bef
vacvvn <vacvvn2012@gmail.com>
2026-09-22 17:41:38 +0300

    chore[форматирование doxygen rcm_oi10_o32t.h]

--- include/hw/ppc/rcm_oi10_o32t.h
+++ include/hw/ppc/rcm_oi10_o32t.h
@@ -15,11 +15,6 @@ OBJECT_DECLARE_SIMPLE_TYPE(O32TState, O32T)
     \brief интерфейсные функции
 */
 /**
-    \defgroup OI10_PERIF_SETTINGS
-    \ingroup OI10
-    \brief настройки периферии СнК ОИ10
-*/
-/**
     \defgroup OI10_DCR_MEMORY_MAP
     \ingroup OI10
     \brief карта памяти  DCR СнК ОИ10
@@ -29,6 +24,11 @@ OBJECT_DECLARE_SIMPLE_TYPE(O32TState, O32T)
     \defgroup OI10_CONFIG
     \ingroup OI10
     \brief конфигурация сборки модели OI10
+*/
+/**
+    \defgroup OI10_IRQ_LINES
+    \ingroup OI10_CONFIG
+    \brief номера линий прерываний СнК ОИ10
 */
 #define OI10_O32T_USE_INTERNAL_ROM 7
 #define OI10_O32T_SD_CARD_INSERTED_GPIO 3
@@ -45,13 +45,13 @@ OBJECT_DECLARE_SIMPLE_TYPE(O32TState, O32T)
 #define WDT_BASE_ADDR              0x800B0000U
 
 /// DIT IRQ line1
-/// \ingroup OI10_PERIF_SETTINGS
+/// \ingroup OI10_CONFIG
 #define OI10_DIT1_IRQ_LINE 40U
 /// DIT IRQ line2
-/// \ingroup OI10_PERIF_SETTINGS
+/// \ingroup OI10_CONFIG
 #define OI10_DIT2_IRQ_LINE 41U
 /// WDT IRQ line
-/// \ingroup OI10_PERIF_SETTINGS
+/// \ingroup OI10_CONFIG
 #define OI10_WDT_IRQ_LINE 42U
 
 /**
@@ -62,7 +62,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(O32TState, O32T)
 * @warning на данный момент от регистров системы тактирования не зависит и
 * зафиксирована на значении 25MHz.
 *
-* @ingroup OI10_PERIF_SETTINGS
+* @ingroup OI10_CONFIG
 */
 #define DOUBLE_TIMER_BASE_FREQ  25000000
 
@@ -73,7 +73,7 @@ OBJECT_DECLARE_SIMPLE_TYPE(O32TState, O32T)
 * @warning на данный момент от регистров системы тактирования не зависит и
 * зафиксирована на значении 25MHz.
 *
-* @ingroup OI10_PERIF_SETTINGS
+* @ingroup OI10_CONFIG
 */
 #define WDT_BASE_FREQ  25000000
     
@@ -94,66 +94,5 @@ BusState *oi10_o32t_get_spi_bus(DeviceState *dev, int spi_num);
  */
 qemu_irq oi10_o32t_get_ext_int_irq(DeviceState *dev, unsigned int_num);
 
-///@}
-//<<<<<<<<<<<<<<<<oi10_interface_api defgroup end<<<<<<<<<<<<<<<<<<<<<<<
-
-//>>>>>>>>>>>>>>>oi10_dcr_memory_map defgroup begin>>>>>>>>>>>>>>>>>>>>>>
-/**
-    \defgroup OI10_DCR_MEMORY_MAP
-    \ingroup 
-    \brief карта памяти  DCR СнК ОИ10
-*/
-///@{
-
-/// базовый адрес контроллера DIT по умолчанию. Можно изменять через свойство baseaddr
-#define DOUBLE_TIMER_BASE_ADDR   0x800A0000U
-
-///@}
-//<<<<<<<<<<<<<<<<oi10_dcr_memory_map defgroup end<<<<<<<<<<<<<<<<<<<<<<<
-
-
-//>>>>>>>>>>>>>>>oi10_perif_settings defgroup begin>>>>>>>>>>>>>>>>>>>>>>
-/**
-    \defgroup OI10_PERIF_SETTINGS
-    \ingroup //todo
-    \brief настройки периферии СнК ОИ10
-*/
-///@{
-///@}
-//<<<<<<<<<<<<<<<<oi10_perif_settings defgroup end<<<<<<<<<<<<<<<<<<<<<<<
-
-//>>>>>>>>>>>>>>>oi10_irq_lines defgroup begin>>>>>>>>>>>>>>>>>>>>>>
-/**
-    \defgroup OI10_IRQ_LINES
-    \ingroup OI10_PERIF_SETTINGS
-    \brief номера линий прерываний СнК ОИ10
-*/
-///@{
-
-/// double timer1 IRQ line
-#define OI10_DIT1_IRQ_LINE 40U
-/// double timer2 IRQ line
-#define OI10_DIT2_IRQ_LINE 41U
-
-///@}
-//<<<<<<<<<<<<<<<<oi10_irq_lines defgroup end<<<<<<<<<<<<<<<<<<<<<<<
-
-//>>>>>>>>>>>>>>>dit_settings defgroup begin>>>>>>>>>>>>>>>>>>>>>>
-/**
-    \defgroup DIT_SETTINGS
-    \ingroup OI10_PERIF_SETTINGS
-    \brief настройки модуля DIT
-*/
-///@{
-
-/// базовая частота контроллера DIT
-/// @details Постоянная базовая частота контроллера. Подается от DCR(CLK_DCR)
-/// Эту частоту каждый таймер может делить на 1, 16, 256
-/// @warning на данный момент от регистров системы тактирования не зависит и
-/// зафиксировано на значении 25MHz.
-#define DOUBLE_TIMER_BASE_FREQ  25000000
-
-///@}
-//<<<<<<<<<<<<<<<<dit_settings defgroup end<<<<<<<<<<<<<<<<<<<<<<<
 
 #endif /* RCM_OI10_H */
